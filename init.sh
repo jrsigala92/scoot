9568920b9fcb2821742465ebaefa2c3f3308d683
jrsigala92 <58497137+jrsigala92@users.noreply.github.com>
2023-06-05 16:50:04 -0600

    Add files via upload
    adding project

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+git clone https://github.com/DevSkillsHQ/frontend-boilerplate-angular.git
+cp frontend-boilerplate-angular/package.json .
+cp frontend-boilerplate-angular/cypress.json .
+cp -r frontend-boilerplate-angular/app* .
+rm -rf frontend-boilerplate-angular
+npm i
+git add cypress.json package* app*
+git commit -m 'Init boilerplate'
+#!/bin/sh
+git clone https://github.com/DevSkillsHQ/frontend-boilerplate-angular.git
+cp frontend-boilerplate-angular/package.json .
+cp frontend-boilerplate-angular/cypress.json .
+cp -r frontend-boilerplate-angular/app* .
+rm -rf frontend-boilerplate-angular
+npm i
+git add cypress.json package* app*
+git commit -m 'Init boilerplate'
+#!/bin/sh
+git clone https://github.com/DevSkillsHQ/frontend-boilerplate-angular.git
+cp frontend-boilerplate-angular/package.json .
+cp frontend-boilerplate-angular/cypress.json .
+cp -r frontend-boilerplate-angular/app* .
+rm -rf frontend-boilerplate-angular
+npm i
+git add cypress.json package* app*
+git commit -m 'Init boilerplate'

b215e297c0cef295a67342e03eb537476741a03e
Jesus Sigala <jsigala@ametrics.org>
2023-06-05 17:41:45 -0600

    adding components and implementing ngrx

--- app-frontend/src/app/app-routing.module.ts
+++ app-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ToDoComponent
+  },
+  {
+    path:'**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -3,18 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { ToDoComponent } from './to-do/to-do.component';
+import { ListComponent } from './to-do/list/list.component';
 import { StoreModule } from '@ngrx/store';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
+
+import * as fromToDoReducer from '../app/store/reducers/todo.reducers';
+import { ToDosEffects } from './store/effects/todo.effects';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    StoreModule.forRoot({}), 
+    StoreModule.forFeature(
+       fromToDoReducer.customerFeatureKey,
+       fromToDoReducer.reducer),
+    EffectsModule.forRoot([ToDosEffects]),
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
